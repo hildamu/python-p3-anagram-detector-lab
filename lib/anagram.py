9a4f857ac618aa7dd3d6cb1693a5a99aaf3c87e9
hildamu <mukamihilda437@gmail.com>
2024-05-24 15:16:14 +0300

    submit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+from collections import Counter
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.counter = Counter(self.word)
+
+    def match(self, words):
+        return [word for word in words if Counter(word.lower()) == self.counter and word.lower() != self.word]
+
+
